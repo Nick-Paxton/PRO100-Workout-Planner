d4aeef75849b0030373d0a0dd5e999afbf9d1621
Nick-Paxton <129334596+Nick-Paxton@users.noreply.github.com>
2023-08-16 18:42:13 -0600

    Scripts added to pug files

--- pug/routes/index.js
+++ pug/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Workout Planner' });
+  res.render('index', { title: 'Workout Planner',
+  js: 'javascripts/index.js' });
 });
 
 module.exports = router;
