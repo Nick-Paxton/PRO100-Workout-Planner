106f05bc10c01adc6cd963d95741e44576759fe5
FriendlyNeighborhoodCoder24 <treyoliver81@gmail.com>
2023-08-24 11:17:38 -0600

    Adding in the layout for Scheduler page

--- pug/app.js
+++ pug/app.js
@@ -11,6 +11,7 @@ var registerRouter = require('./routes/register')
 var loginRouter = require('./routes/login')
 var logoutRouter = require('./routes/logout')
 var communityRouter = require('./routes/community')
+var schedulerRouter = require('./routes/scheduler')
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 app.use('/community', communityRouter);
+app.use('/scheduler', schedulerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
