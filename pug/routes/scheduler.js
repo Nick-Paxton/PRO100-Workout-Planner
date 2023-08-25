db33443245fd4b0176854b9acec0d332e3e5ba42
will578 <wharris@student.neumont.edu>
2023-08-24 18:46:25 -0600

    Preemptive fix for scheduler page breaking

--- pug/routes/scheduler.js
+++ pug/routes/scheduler.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.render('scheduler', {title: 'Scheduler'})
+    res.render('scheduler', {title: 'Scheduler', loggedIn: req.session.loggedIn})
 })
 
 module.exports = router
