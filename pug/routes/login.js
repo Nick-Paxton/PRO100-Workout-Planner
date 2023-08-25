2c6a6b35fd9d870d20f74478d1d61f983ca57ac0
will578 <wharris@student.neumont.edu>
2023-08-24 18:41:30 -0600

    Added full session functionality

--- pug/routes/login.js
+++ pug/routes/login.js
@@ -2,15 +2,15 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.render('login', {title: 'Login'})
+    res.render('login', {title: 'Login', loggedIn: req.session.loggedIn})
 })
 
+//currently only sets variable in session to maintain login status
+//todo: add actual login logic with calls to database
 router.post('/', async function(req, res, next) {
     let data = req.body
     req.session.loggedIn = data['username']
     console.log("logged in: " + req.session.loggedIn)
-    console.log("redirecting")
-    console.log(data)
     res.redirect('/')
 })
 
