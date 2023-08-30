7312d6ef38c6a7638dfcc8b1c3a45ef7a1c93939
will578 <wharris@student.neumont.edu>
2023-08-30 09:11:36 -0600

    Reverted redirect change because pug blew up

--- pug/routes/index.js
+++ pug/routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (!req.session.loggedIn) {
-    res.redirect('http://localhost:3000/community/')
-  }
   console.log('current user: ' + req.session.loggedIn)
   res.render('index', { title: 'Workout Planner', 
   loggedIn: req.session.loggedIn,
