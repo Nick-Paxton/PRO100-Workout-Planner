d0c598a931c2a56a1548f40aa85b833eb49e8cbe
Nick-Paxton <129334596+Nick-Paxton@users.noreply.github.com>
2023-08-30 09:18:33 -0600

    Added retrieving of user data on the home screen

--- pug/public/javascripts/index.js
+++ pug/public/javascripts/index.js
@@ -1,26 +1,22 @@
 //var c = require('./calendar')
 currentUser = -1
 
-var getUserID = async () => {
+var getUserID = async (callback) => {
     res = await fetch("http://localhost:3000/users")
     r = await res.json()
     currentUser = r['userID']
-    console.log('user gotten: ' + r['userID'])
+    callback(currentUser)
 }
 var addWeeklyWorkout = (o, workout, year, month, weekday) => {
     d9 = new Date()
-    console.log("day of the week: " + d9.getDay())
     for (week in o[year][month]){
         for (day in o[year][month][week]) {
             d9.setDate(day)
-            console.log("day of the week: " + d9.getDay())
             if (d9.getDay() == (weekday - 1)) {
                 o[year][month][week][day]['workouts'].push(workout)
-                console.log("workout added")
             }
         }
     }
-    console.log(o)
     return o
 }
 
@@ -36,49 +32,54 @@ var workoutsToBeSaved = [
 ]
 var myWorkoutData = null
 
-// fetch("http://localhost:2718/workout/1234")
-//     .then((response) => {
-//         if (response.ok) {
-//             response.json().then((data) => {
-//                 var currentDate = new Date()
-//                 currentDate.setDate(7)
-//                 var firstOfMonth = new Date()
-//                 var firstOfWeek = new Date()
+getUserID((userID) => {
+    fetch("http://localhost:2718/workout/"+userID)
+    .then((response) => {
+        if (response.ok) {
+            response.json().then((data) => {
+                var currentDate = new Date()
+                currentDate.setDate(7)
+                var firstOfMonth = new Date()
+                var firstOfWeek = new Date()
 
-//                 firstOfWeek.setDate(currentDate.getDate()-currentDate.getDay())
-//                 firstOfMonth.setDate(1)
-//                 var daysLeft = 31 - (7 - firstOfMonth.getDay())
-//                 var weekCheck = 31 - firstOfWeek.getDate() + 1
+                firstOfWeek.setDate(currentDate.getDate()-currentDate.getDay())
+                firstOfMonth.setDate(1)
+                var daysLeft = 31 - (7 - firstOfMonth.getDay())
+                var weekCheck = 31 - firstOfWeek.getDate() + 1
                 
-//                 const year = firstOfWeek.getFullYear()
-//                 const month = firstOfWeek.getMonth() + 1
-//                 var currentWeek
-//                 if (weekCheck < daysLeft) currentWeek = 1
-//                 //                         (( 6            - (7 - 2) - 1 ) / 7 = 2
-//                 //                         (( 13           - (7 - 2) - 1 ) / 7) = 3
-//                 else currentWeek = (((firstOfWeek.getDate() - (7 - firstOfMonth.getDay()))-1)/7) + 2
+                const year = firstOfWeek.getFullYear()
+                const month = firstOfWeek.getMonth() + 1
+                var currentWeek
+                if (weekCheck < daysLeft) currentWeek = 1
+                //                         (( 6            - (7 - 2) - 1 ) / 7 = 2
+                //                         (( 13           - (7 - 2) - 1 ) / 7) = 3
+                else currentWeek = (((firstOfWeek.getDate() - (7 - firstOfMonth.getDay()))-1)/7) + 2
 
 
-//                 for (var i = 0; i < 7; i++) {
-//                     var day = firstOfWeek.getDate() + i
-//                     var dayOfWeek = firstOfWeek.getDay() + i
+                for (var i = 0; i < 7; i++) {
+                    var day = firstOfWeek.getDate() + i
+                    var dayOfWeek = firstOfWeek.getDay() + i
 
-//                     var ul = document.querySelector("#list"+dayOfWeek)
+                    var ul = document.querySelector("#list"+dayOfWeek)
+                    if (data[year+""][month+""][currentWeek+""][day+""]["workouts"]) {
 
-//                     data[year+""][month+""][currentWeek+""][day+""]["workouts"].forEach(element => { // ***** need to figure out how to handle empty objects *****
-//                         var exercise = element['name'] + ': Reps: ';
-//                         exercise = exercise + element['reps'] + ': Sets: ';
-//                         exercise = exercise + element['sets']
-//                         // place the data on the UI
+                        data[year+""][month+""][currentWeek+""][day+""]["workouts"].forEach(element => { // ***** need to figure out how to handle empty objects *****
+                            var exercise = element['name'] + ': Reps: ';
+                            exercise = exercise + element['reps'] + ': Sets: ';
+                            exercise = exercise + element['sets']
+                            // place the data on the UI
 
-//                         var li = document.createElement('li')
-//                         li.innerHTML = exercise
-//                         ul.appendChild(li)
-//                     });
-//                 }
-//             })
-//         }
-//     })
+                            var li = document.createElement('li')
+                            li.innerHTML = exercise
+                            ul.appendChild(li)
+                        });
+                    }
+                }
+            })
+        }
+    })
+})
+
 
 const shareButton = document.getElementById('share-button')
 shareButton.addEventListener('click', shareWorkouts)
@@ -118,14 +119,15 @@ async function shareWorkouts() {
 
     let liElements = dayList.getElementsByTagName("li")
     let title = document.getElementById('title')
-    console.log(title.value)
 
     if (title.value === "") {
         title.place = "PLEASE ENTER A TITLE"
         return
     }
 
-    await getUserID()
+    await getUserID((d) => {
+        console.log("user gotten: ", d)
+    })
 
     let communityPost = {
         userID: currentUser,
@@ -146,8 +148,6 @@ async function shareWorkouts() {
         communityPost["workouts"].push(workout)
     }
 
-    console.log(communityPost)
-
 
     await fetch("http://localhost:2718/community/", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
@@ -159,7 +159,6 @@ async function shareWorkouts() {
             if (response.ok) {
                 console.log("OK")
             }
-            console.log(myWorkoutData)
     })
 
     closeTitlePop()
@@ -171,7 +170,9 @@ saveButton.addEventListener('click', saveWorkouts)
 async function saveWorkouts() {
     let myWorkoutData
     
-    await getUserID()
+    await getUserID((d) => {
+        console.log("user gotten: ", d)
+    })
 
     await fetch("http://localhost:2718/workout/" + currentUser).then(async (response) => {
         if(response.ok) {
@@ -218,7 +219,6 @@ async function saveWorkouts() {
             if (response.ok) {
                 console.log("OK")
             }
-            console.log(myWorkoutData)
     })
         
 }
